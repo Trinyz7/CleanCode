ffc1a097d2350428925d0c9bf9e9c0e15c6ddb8d
Anthony <anthony71.nguyen@gmail.com>
2024-12-13 09:27:56 +0100

    activité 6

--- finish/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
+++ finish/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
@@ -1,32 +1,40 @@
 package com.cleancode.knuth;
 
 public class NumberPrinter {
-    private int linesPerPage;
-    private int columns;
+    private final int linesPerPage;
+    private final int columns;
 
     public NumberPrinter(int linesPerPage, int columns) {
         this.linesPerPage = linesPerPage;
         this.columns = columns;
     }
 
-    public void print(int[] numbers, int numberOfNumbers) {
-        int pagenumber = 1;
-        int pageoffset = 1;
+    public void printNumbers(int[] numbers, int numberOfNumbers) {
+        int pageoffset = 1; // Explicit declaration
+        int pagenumber = 1; // Explicit declaration
+
         while (pageoffset <= numberOfNumbers) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(numberOfNumbers));
-            System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(pagenumber));
+            System.out.print(numberOfNumbers);
+            System.out.print(" Numbers === Page ");
+            System.out.print(pagenumber);
             System.out.println("\n");
-            for (int rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage -1; rowoffset++) {
-                for (int column = 0; column <= columns - 1; column++)
-                    if (rowoffset + column * linesPerPage <= numberOfNumbers)
+            for (int rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
+                for (int column = 0; column <= columns - 1; column++) {
+                    if (rowoffset + column * linesPerPage <= numberOfNumbers) {
                         System.out.printf("%10d", numbers[rowoffset + column * linesPerPage]);
+                    }
+                }
                 System.out.println();
             }
             System.out.println("\f");
             pagenumber++;
             pageoffset += linesPerPage * columns;
         }
+    }
+
+    public void print(int[] primes, int numberOfPrimes) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'print'");
     }
 }
